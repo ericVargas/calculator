0fadbf4cf5402178bcaa3ba0bc97d928e4139481
ericVargas <eric.vargas719@gmail.com>
2019-01-12 16:26:23 -0500

    placed footer and changed bg

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 // TIME
-let d = new Date();
-let time = document.getElementById('time');
-let currentTime = d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true});
+function updateTime() {
+    let d = new Date();
+    let time = document.getElementById('time');
+    let currentTime = d.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+    });
 
-time = currentTime;
+    time.innerHTML = currentTime;
+};
+
+setInterval(updateTime, 1000);
 
 // POWER
 let power = document.getElementById('power');
 
-navigator.getBattery().then(function(battery) {
+navigator.getBattery().then(function (battery) {
     let level = battery.level;
-    console.log(level);
     let currentPower = (level * 100) + "%";
     power.innerHTML = currentPower;
 });
